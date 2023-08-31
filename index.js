d60486c7a75fb9e432d1cebeb4eb997f3a6de3db
Shashank Sharma <shashank.hacky0@gmail.com>
2023-08-31 14:02:55 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// preloader
+var loader = document.getElementById("preloader");
+window.addEventListener("load", function () {
+  loader.style.display = "none";
+});
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var btn2 = document.getElementById("myBtn2");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+btn2.onclick = function () {
+  modal.style.display = "block";
+};
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
